31322719c1f27f9158aebaf0c4a0e589247ea790
nyanp <nyanpn@gmail.com>
2018-07-29 20:18:46 +0900

    m19 - One-Hot追加 - CV796450

--- feature/prev.py
+++ feature/prev.py
@@ -21,6 +21,12 @@ class Prev(object):
         self.df['DAYS_LAST_DUE'].replace(365243, np.nan, inplace=True)
         self.df['DAYS_TERMINATION'].replace(365243, np.nan, inplace=True)
 
+    def _target_encoding(self, col, name):
+        tgt = self.df.groupby(col)['TARGET'].mean().reset_index()
+
+        tgt.columns = [col, name]
+        self.df = pd.merge(self.df, tgt, on=col, how='left')
+
     def transform(self):
         if self.transformed:
             return
@@ -30,10 +36,18 @@ class Prev(object):
 
         self.df = pd.concat([self.df, pd.get_dummies(self.df['NAME_YIELD_GROUP'], prefix='NAME_YIELD_GROUP')], axis=1)
 
+        self.df = pd.concat([self.df, pd.get_dummies(self.df['NAME_GOODS_CATEGORY'], prefix='NAME_GOODS_CATEGORY')], axis=1)
+        self.df = pd.concat([self.df, pd.get_dummies(self.df['PRODUCT_COMBINATION'], prefix='PRODUCT_COMBINATION')], axis=1)
+
+        #app = pd.read_feather('../input/application_all.f')
+        #self.df = pd.merge(self.df, app[['SK_ID_CURR', 'TARGET']], on='SK_ID_CURR', how='left')
+
+        # target encoding
+        #self._target_encoding('NAME_GOODS_CATEGORY', 'TGT_NAME_GOODS_CATEGORY')
+        #self._target_encoding('PRODUCT_COMBINATION', 'TGT_PRODUCT_COMBINATION')
+
         self.df.to_feather('cache/prev.f')
         self.transformed = True
-
-        app = pd.read_feather('../input/')
 
     @classmethod
     def from_cache(cls):
@@ -61,8 +75,14 @@ class Prev(object):
             'NAME_YIELD_GROUP_high': ['mean'],
             'NAME_YIELD_GROUP_low_action': ['mean'],
             'NAME_YIELD_GROUP_low_normal': ['mean'],
-            #'NAME_YIELD_GROUP_middle': ['sum'],
-            #'NAME_YIELD_GROUP_XNA': ['sum']
+
+            'NAME_YIELD_GROUP_middle': ['mean'],
+            'NAME_YIELD_GROUP_XNA': ['mean'],
+
+            'NAME_GOODS_CATEGORY_XNA': ['mean'],
+            'NAME_GOODS_CATEGORY_Mobile': ['mean'],
+            'NAME_GOODS_CATEGORY_Consumer Electronics': ['mean'],
+            'NAME_GOODS_CATEGORY_Computers': ['mean'],
         }
 
         df_base = features_common.aggregate(df_base, agg, self.df, 'p_')
