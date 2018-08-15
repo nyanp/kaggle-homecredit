046b2ef58232645ea85a437b97b79908de77a844
Taiga Noumi <Noumi.Taiga@gmail.com>
2018-08-15 23:34:27 +0900

    fix

--- feature/add0815.py
+++ feature/add0815.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-%matplotlib inline
 
 df = pd.read_feather('input/application_all.f')
 install = pd.read_feather('input/installments_payments.csv.f')
