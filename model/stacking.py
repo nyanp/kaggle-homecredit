c4129eadc507d28a2d08a5a403a9b3b856cfc451
nyanp <nyanpn@gmail.com>
2018-08-28 01:23:12 +0900

    lgbm seed averaging for 1st-stage

--- model/stacking.py
+++ model/stacking.py
@@ -3,27 +3,33 @@ import numpy as np
 
 debug=False
 
-def train_1st_stage(remove_list = None, postfix = '', param=None):
+def train_1st_stage(remove_list = None, postfix = '', param=None, averaging=1):
     print('param: {}'.format(param))
     if debug:
         n_est = 10
     else:
         n_est = 10000
 
-    m = lgbm.LGBM(name='stack_1st_{}'.format(postfix), remove_prefix_list = remove_list, param=param, n_estimators=n_est)
-
-    _, auc, train1, test1 = m.cv()
-
-    print('{} : auc {}'.format(postfix, auc))
-
-    np.save('train_{}'.format(postfix), train1)
-    np.save('test_{}'.format(postfix), test1)
+    if averaging == 1:
+        m = lgbm.LGBM(name='stack_1st_{}'.format(postfix), remove_prefix_list = remove_list, param=param, n_estimators=n_est)
+        _, auc, train1, test1 = m.cv()
+        print('{} : auc {}'.format(postfix, auc))
+        np.save('train_{}'.format(postfix), train1)
+        np.save('test_{}'.format(postfix), test1)
+    else:
+        for i in range(averaging):
+            m = lgbm.LGBM(name='stack_1st_{}_seed{}'.format(postfix, i), remove_prefix_list=remove_list, param=param,
+                          n_estimators=n_est, lgb_seed=i)
+            _, auc, train1, test1 = m.cv()
+            print('{} : auc {}'.format(postfix, auc))
+            np.save('train_{}_seed{}'.format(postfix,i), train1)
+            np.save('test_{}_seed{}'.format(postfix,i), test1)
 
 
 train_1st_stage(None, 'full')
-train_1st_stage(['ins','p_','pos','b_','credit'], 'app')
-train_1st_stage(['ins','pos','b_','credit'], 'app_prev')
-train_1st_stage(['p_','pos','b_','credit'], 'app_ins')
-train_1st_stage(['ins','p_','b_','credit'], 'app_pos')
-train_1st_stage(['ins','p_','pos','credit'], 'app_bureau')
-train_1st_stage(['ins','p_','pos','b_'], 'app_credit')
+train_1st_stage(['ins','p_','pos','b_','credit'], 'app', averaging=10)
+train_1st_stage(['ins','pos','b_','credit'], 'app_prev', averaging=10)
+train_1st_stage(['p_','pos','b_','credit'], 'app_ins', averaging=10)
+train_1st_stage(['ins','p_','b_','credit'], 'app_pos', averaging=10)
+train_1st_stage(['ins','p_','pos','credit'], 'app_bureau', averaging=10)
+train_1st_stage(['ins','p_','pos','b_'], 'app_credit', averaging=10)
