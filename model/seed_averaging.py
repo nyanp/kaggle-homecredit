96f45cbbd49102f98700a99bf614f31aab3d6636
nyanp <nyanpn@gmail.com>
2018-08-27 23:58:57 +0900

    subsampling for seed averaging

--- model/seed_averaging.py
+++ model/seed_averaging.py
@@ -6,12 +6,16 @@ if __name__ == "__main__":
 
     n_seeds = 10 if argc == 1 else int(sys.argv[1])
     debug = 0 if argc < 3 else int(sys.argv[2])
+    sample = 0 if argc < 4 else int(sys.argv[3])
 
-    print('n_seeds:{}, debug:{}'.format(n_seeds, debug))
+    print('n_seeds:{}, debug:{}, sample:{}'.format(n_seeds, debug, sample))
 
     for i in range(n_seeds):
-        name = 'lgbm_seed{}'.format(i)
+        if sample > 0:
+            name = 'lgbm_sample{}_seed{}'.format(sample, i)
+        else:
+            name = 'lgbm_seed{}'.format(i)
 
-        m = lgbm.LGBM(name, lgb_seed=i, n_estimators=100 if debug else 10000)
+        m = lgbm.LGBM(name, lgb_seed=i, n_estimators=100 if debug else 10000, undersample=sample)
 
         m.cv(save_oof='../stack/{}_'+name+'.npy')
