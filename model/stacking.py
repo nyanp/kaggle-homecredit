735dd4c094ba5fa54ecb2dbc7c8e2934a7e95d77
nyanp <Noumi.Taiga@gmail.com>
2018-08-28 17:47:38 +0900

    Update stacking.py

--- model/stacking.py
+++ model/stacking.py
@@ -19,12 +19,12 @@ def train_1st_stage(remove_list = None, postfix = '', param=None, averaging=1):
         np.save('test_{}'.format(postfix), test1)
     else:
         for i in range(averaging):
-            m = lgbm.LGBM(name='stack_1st_{}_seed{}_uc'.format(postfix, i), remove_prefix_list=remove_list, param=param,
-                          n_estimators=n_est, lgb_seed=i, undersample=8000)
+            m = lgbm.LGBM(name='stack_1st_{}_seed{}_dart'.format(postfix, i), remove_prefix_list=remove_list, param=param,
+                          n_estimators=n_est, lgb_seed=i)
             _, auc, train1, test1 = m.cv()
             print('{} : auc {}'.format(postfix, auc))
-            np.save('train_{}_seed{}_uc'.format(postfix,i), train1)
-            np.save('test_{}_seed{}_uc'.format(postfix,i), test1)
+            np.save('train_{}_seed{}_dart'.format(postfix,i), train1)
+            np.save('test_{}_seed{}_dart'.format(postfix,i), test1)
             del m
             gc.collect()
 
